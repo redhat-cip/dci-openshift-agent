741952f1c828170dd54a330ad44eb2df2a2c23ce
Thomas Vassilian <tvassili@redhat.com>
2018-09-21 15:16:26 -0400

    [WIK] Draft for dci-openshift-agent
    Change-Id: Ia40b74b13a1dcde06f0184069ace6c5d29112300

---
+++ dcirc.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DCI_CLIENT_ID='remoteci/<UUID>'
+DCI_API_SECRET='<PASSWORD>'
+DCI_CS_URL='https://api.distributed-ci.io/'
+
+export DCI_CLIENT_ID
+export DCI_API_SECRET
+export DCI_CS_URL
