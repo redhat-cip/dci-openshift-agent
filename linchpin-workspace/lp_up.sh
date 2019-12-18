cb2fb0a904865649918d7f437f607bb41a6bc76a
Bill Peck <bill@pecknet.com>
2019-12-17 19:23:14 -0500

    WIP - Example install of OCP using emulated bare metal in libvirt
    Still need to figure out where to tie into ocp agent.
    
    Change-Id: Ie1ecd7de00f930053523e28a9831df9cd0830bad

---
+++ linchpin-workspace/lp_up.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+
+linchpin --template-data @libvirt_settings.yml -v up libvirt-network libvirt-hosts
+
+ansible-playbook -i inventories/ocp-edge.inventory \
+                 -e @libvirt_settings.yml \
+                 hooks/ansible/ocp-edge-setup/baremetal-deploy-vm.yml
