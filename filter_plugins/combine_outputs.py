cc846afaeab7c3054ded74a7ab6fbdc76a27cdf2
Bill Peck <bill@pecknet.com>
2020-08-24 14:39:58 -0400

    Fix playbook to copy outputs for pipeline
    Change-Id: I6fa1968131ca7ae655c58ec77e1a0929e5e15450

---
+++ filter_plugins/combine_outputs.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+from ansible.errors import AnsibleError, AnsibleFilterError
+from ansible.module_utils._text import to_native
+
+def combine_outputs_filter(input1, input2):
+    combined = {}
+    for item in input1.items():
+        try:
+            input2_value = input2[item[0]]
+        except KeyError as e:
+            raise AnsibleFilterError("Missing key in Dictionary: %s" % to_native(e))
+        combined_items = [item[1], input2_value]
+        combined[item[0]] = combined_items
+    return combined
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'combine_outputs': combine_outputs_filter,
+        }
