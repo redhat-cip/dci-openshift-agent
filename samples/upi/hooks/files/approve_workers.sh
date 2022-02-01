07bb8657b9c1c53f24d419f8af3b68971905d607
Bill Peck <bill@pecknet.com>
2022-02-01 17:07:26 -0500

    General improvments for UPI support
    The x86 live iso is needed in order to install the UEFI shim for
    netboot.
    
    if ipmi_uefi=true is specified in node inventory then use EFI options
    for IPMI commands.
    
    Update sample install-config template to set cluster domain and name
    from inventory file.
    
    Don't start firewalld in vbmc role, leave it up to the admin.
    
    Minor updates to csr approval routine as well.
    
    Move all install tasks into sample hook.  Partners can use all or none
    of the sample, leave the implementation completely up to them.
    
    Change-Id: Id71da03f86835da1f3dace270a69f31425eeb517

--- samples/upi/hooks/files/approve_workers.sh
+++ samples/upi/hooks/files/approve_workers.sh
@@ -2,6 +2,12 @@
 
 PATH=$1:$PATH
 NUM_WORKERS=$2
+export KUBECONFIG=$3
+
+if [ ! -e "$KUBECONFIG" ]; then
+   echo "$KUBECONFIG not found!"
+   exit 1
+fi
 
 CSRS=$(oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' | wc -l)
 while [ $CSRS -lt $NUM_WORKERS ]; do
